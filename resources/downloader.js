d9aaaac04a9ad28168fb29f39d1ea44fd6ea7362
Vitor Martins <vitomartinsalmeida@gmail.com>
2024-06-18 19:41:42 -0300

    creating downloader and converter

---
+++ resources/downloader.js
@@ -0,0 +1,37 @@
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpegPath = require('ffmpeg-static');
+const ytdl = require('ytdl-core');
+const path = require('path');
+const fs = require('fs');
+
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+async function downloadAndConvertToMp3(youtubeUrl, outputFilePath) {
+  const videoStream = ytdl(youtubeUrl, { quality: 'highestaudio' });
+  const tempFilePath = path.resolve(__dirname, 'temp-video.mp4');
+
+  const tempFileStream = fs.createWriteStream(tempFilePath);
+
+  videoStream.pipe(tempFileStream);
+
+  videoStream.on('end', () => {
+    ffmpeg(tempFilePath)
+      .output(outputFilePath)
+      .on('end', () => {
+        console.log('Conversion finished');
+        fs.unlinkSync(tempFilePath); // Delete the temporary video file
+      })
+      .on('error', (err) => {
+        console.error('Error:', err);
+        fs.unlinkSync(tempFilePath); // Delete the temporary video file in case of an error
+      })
+      .run();
+  });
+
+  videoStream.on('error', (err) => {
+    console.error('Error downloading the video:', err);
+    fs.unlinkSync(tempFilePath);
+  });
+}
+
+module.exports = { downloadAndConvertToMp3 };
